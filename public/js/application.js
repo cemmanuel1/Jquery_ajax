a36097c8749b3f476c5377fb22e2bd01480fd4fa
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-07-05 08:33:05 -0700

    completed

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-
+  $("#get_color").on("click",function(){
+    $.ajax({
+      method: "POST",
+      url: "/color"
+    }).done(function(response){
+      $("ul li").eq(response.cell).css("background-color", response.color)
+    });
+  });
 });
+
+//Can also be written as: 
+// $(document).ready(function(){
+//   $('#get_color').on('click', function(e) {
+//     e.preventDefault();
+//     $.post('/color').done(function(response) {
+//       $('ul li').eq(response.cell).css('background-color', response.color);
+//     });
+//   });
+// });
